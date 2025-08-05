e70cee65d410fd6f59e530ae5eeec114de8717de
Bunga Pravalika <pravalikabunga@gmail.com>
2025-08-06 03:05:52 +0530

    In footer, copy rights 2024 changed to 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -82,7 +82,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-gray-700 mt-12 pt-8 text-center text-gray-300">
-          <p>&copy; 2024 ProTax by KC & Associates. All rights reserved.</p>
+          <p>&copy; 2025 ProTax by KC & Associates. All rights reserved.</p>
         </div>
       </div>
     </footer>
